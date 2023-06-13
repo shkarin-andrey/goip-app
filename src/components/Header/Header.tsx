3d6242459737507a3cdb3d95c24ab7504c2f914d
Andrey Shkarin <shkarinandrew@mail.ru>
2023-06-13 16:53:40 +0300

    Закрыть приложение и подгрузка юзера

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import { FC } from 'react';
 
 import SettingsIcon from '../../assets/icons/SettingsIcon';
+import { useWebTelegramApp } from '../../hooks/useWebTelegramApp';
 
 const Header: FC = () => {
+  const { onClose } = useWebTelegramApp();
+
   return (
     <div className='px-4 py-[14px] flex justify-between items-center bg-[#1F283C] text-white text-[17px]'>
-      <div className='text-[#5AC8FB] cursor-pointer'>Отмена</div>
+      <button className='text-[#5AC8FB] cursor-pointer' onClick={onClose}>
+        Отмена
+      </button>
       <div className='flex flex-col items-center'>
         <div className='font-semibold'>VolPLab</div>
         <div className='text-[#6C6C6C] text-xs'>бот</div>
