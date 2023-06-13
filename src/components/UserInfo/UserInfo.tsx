9289e4b0f71254a957a89c51a5ec4133f7fbc5b9
Andrey Shkarin <shkarinandrew@mail.ru>
2023-06-13 17:00:19 +0300

    test

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -11,18 +11,33 @@ const UserInfo: FC = () => {
   const user = onDataUnsafe().user;
 
   return (
-    <div className='flex items-center gap-3 px-4 pb-4 bg-[#1F283C]'>
-      <div className='w-[38px] h-[38px] rounded-full overflow-hidden'>
-        <img src={user?.photo_url} alt='avatar' className='w-full h-full object-cover' />
-      </div>
-
-      <div className='flex flex-col'>
-        <div className='text-[17px] text-white font-medium leading-none'>
-          {user?.first_name} {user?.last_name}
+    <>
+      <div className='flex items-center gap-3 px-4 pb-4 bg-[#1F283C]'>
+        <div className='w-[38px] h-[38px] rounded-full overflow-hidden'>
+          <img
+            src={user?.photo_url}
+            alt='avatar'
+            className='w-full h-full object-cover'
+          />
         </div>
-        <div className='text-[#5AC8FB] text-[13px]'>{user?.usernames}</div>
+
+        <div className='flex flex-col'>
+          <div className='text-[17px] text-white font-medium leading-none'>
+            {user?.first_name} {user?.last_name}
+          </div>
+          <div className='text-[#5AC8FB] text-[13px]'>{user?.usernames}</div>
+        </div>
       </div>
-    </div>
+      <div>
+        {user?.first_name}
+        {user?.id}
+        {user?.is_bot}
+        {user?.language_code}
+        {user?.last_name}
+        {user?.photo_url}
+        {user?.usernames}
+      </div>
+    </>
   );
 };
 
